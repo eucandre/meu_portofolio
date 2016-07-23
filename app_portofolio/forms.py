f74e0bcefe5a0f0fc77d796fa23603eebee57507
eucandre <eucandre@gmail.com>
2016-07-23 01:33:37 -0300

    ajustando o formulário

--- app_portofolio/forms.py
+++ app_portofolio/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from models import *
 class Formcontato(forms.Form):
-    first_name   = forms.CharField(max_length=150)
-    last_name    = forms.CharField(max_length=150)
-    email        = forms.EmailField()
-    phone        = forms.CharField(150)
-    message_text = forms.CharField(widget=forms.Textarea)
+    first_name   = forms.CharField(max_length=150, widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Nome *"}))
+    last_name    = forms.CharField(max_length=150, widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Sobrenome *"}))
+    email        = forms.EmailField( widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Email *"}))
+    phone        = forms.CharField(max_length=150, widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Tel."}))
+    message_text = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control","placeholder":"Mensagem *"}), max_length=500)
--- app_portofolio/views.py
+++ app_portofolio/views.py
@@ -5,13 +5,12 @@ from app_portofolio.models import *
 
 def portofolio(request):
     if request.method == 'POST':
-        form = Formcontato(request.FILES, request.POST)
+        form = Formcontato(request.POST, request.FILES)
         if form.is_valid():
             dados = form.cleaned_data
             item  = contato(first_name = dados['first_name'], last_name = dados['last_name'], email=dados['email'], phone=dados['phone'], message_text=dados['message_text'])
             item.save()
-            mostra = item.first_name
-            return mostra
+            return render_to_response("salvo.html",{})
     else:
-        form = Formcontato()
+            form = Formcontato()
     return render_to_response("index.html",{"form": form}, RequestContext(request))
